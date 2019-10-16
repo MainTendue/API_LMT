273dff2f03dce981886f1379bd6f83c438a5324f
Alain Roger SANGA <ar.sanga@sciencesu-lyon.fr>
2019-10-16 10:03:18 +0200

    suppression de projection

--- src/main/java/com/lmtAPI/lmt/Repository/UserRepository.java
+++ src/main/java/com/lmtAPI/lmt/Repository/UserRepository.java
@@ -1,10 +1,8 @@
 package com.lmtAPI.lmt.Repository;
 
 import com.lmtAPI.lmt.model.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByEmail(@Param("email") String email);
